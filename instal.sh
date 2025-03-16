81471a44df783fe3cd3ea43b31c9462d2135e23c
Daniel Wyrwał <daniel.wyrwal@gmail.com>
2025-03-16 20:53:37 +0100

    update after fedora

--- instal.sh
+++ instal.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-sudo apt install hyprland nemo waybar sway-notification-center blueman fuzzel alacritty zsh swaylock pavucontrol pulseaudio ranger grim slurp wl-clipboard
+# sudo apt install hyprland nemo waybar sway-notification-center blueman fuzzel alacritty zsh swaylock pavucontrol pulseaudio ranger grim slurp wl-clipboard
+
+rm ~/.config/hypr -rf
+stow hypr
+
+rm ~/.config/waybar -rf
+stow waybar
+
+rm ~/.config/kitty -rf
+stow kitty
+
+rm ~/.config/starship -rf
+stow starship
+
+rm ~/.zshrc 
+stow zshrc
+
+rm /etc/logid.cfg 
+sudo stow logid --target=/
